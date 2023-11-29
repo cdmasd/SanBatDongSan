782d4ec5aa584b5ec1ff02998f33efe6e661fbde
cdmasd <120780060+cdmasd@users.noreply.github.com>
2023-11-29 19:09:09 +0700

    Update Mail.cs

--- BUS_QuanLyBDS/Mail.cs
+++ BUS_QuanLyBDS/Mail.cs
@@ -289,5 +289,100 @@ namespace BUS_QuanLyBDS
                 client.Send(Msg);
             }
         }
+        public static void MailPayment(string toEmail,string name,string tiennap,string sodu)
+        {
+            // Khởi tạo đối tượng MailMessage để tạo email.
+            MailMessage Msg = new MailMessage();
+
+            // Đặt người gửi email.
+            Msg.From = new MailAddress("tienphanps28044@gmail.com");
+
+            // Đặt người nhận email, thường là địa chỉ email của người dùng muốn khôi phục mật khẩu.
+            Msg.To.Add(toEmail.Trim());
+
+            // Đặt tiêu đề của email.
+            Msg.Subject = "Thanh toán thành công";
+
+            // Đặt nội dung (body) của email và chèn mật khẩu mới vào nội dung email.
+            Msg.Body = @"<!DOCTYPE html>
+<html>
+<head>
+    <meta charset=""utf-8"">
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+        }
+        .container {
+            background-color: #ffffff;
+            width: 80%;
+            max-width: 6000px;
+            margin: 20px auto;
+            padding: 20px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+        .header {
+            background-color: #33CCFF;
+            padding: 10px 20px;
+            text-align: center;
+            font-size: 24px;
+            font-weight: bold;
+        }
+        .content {
+            padding: 20px;
+            text-align: center;
+            line-height: 1.6;
+        }
+        .footer {
+            background-color: #eeeeee;
+            padding: 10px 20px;
+            text-align: center;
+            font-size: 12px;
+        }
+        .confirmation-code {
+      		text-align: left;
+            font-size: 15px;
+            color: #333;
+            margin: 20px 0;
+            padding: 10px;
+      		min-width : 200px;
+            border: 1px dashed #333;
+            display: inline-block;
+        }
+    </style>
+</head>
+<body>
+    <div class=""container"">
+        <div class=""header"">
+            Thanh toán thành công
+        </div>
+        <div class=""content"">
+            <div class=""confirmation-code"">
+              	<strong>Email : " + toEmail + @"</strong><br>
+                <strong>Họ tên : " + name + @"</strong><br>
+                <strong>Số tiền nạp : " + tiennap + @"</strong><br>
+                <strong>Số dư còn lại : " + sodu + @"</strong>
+            </div>
+        </div>
+        <div class=""footer"">
+            Đây là email tự động, vui lòng không trả lời. Nếu bạn cần hỗ trợ, xin vui lòng liên hệ sử dụng chức năng hỗ trợ trong ứng dụng.
+        </div>
+    </div>
+</body>
+</html>";
+            Msg.IsBodyHtml = true;
+            using (SmtpClient client = new SmtpClient())
+            {
+                client.EnableSsl = true;
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential("tienphanps28044@gmail.com", "oxap pttt nkmb yvuu");
+                client.Host = "smtp.gmail.com";
+                client.Port = 587;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.Send(Msg);
+            }
+        }
     }
 }
